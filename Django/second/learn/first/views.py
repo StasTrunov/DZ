f6f03861cae4be34eafd5ff47bfb645b75eabcd0
Stanislav <stasjazz10@gmail.com>
2023-03-25 20:32:57 +0000

    first edition

--- Django/second/learn/first/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products_list.html', {"products": products})
